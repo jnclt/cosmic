5c57d1e01fe663fd1a0bb00f99871ccba9ecd91f
Jan Calta <jan.calta@here.com>
2022-06-13 22:28:46 +0200

    Add test for allocate

---
+++ test_model.py
@@ -0,0 +1,8 @@
+
+def test_allocate_reduces_stock():
+    batch = Batch('ref1', 'sku-table', 10)
+    order_line = OrderLine('sku_table', 2)
+
+    batch.allocate(order_line)
+
+    assert batch.quantity == 8
