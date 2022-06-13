eeb22383b18e9fa003454193ed6ae0fc78d4cb00
Jan Calta <jan.calta@here.com>
2022-06-13 23:36:44 +0200

    Add poetry setup

--- test_model.py
+++ test_model.py
@@ -1,7 +1,6 @@
-
 def test_allocate_reduces_stock():
-    batch = Batch('ref1', 'sku-table', 10)
-    order_line = OrderLine('sku_table', 2)
+    batch = Batch("ref1", "sku-table", 10)
+    order_line = OrderLine("sku_table", 2)
 
     batch.allocate(order_line)
 
