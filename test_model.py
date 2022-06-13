b430fe52acc22c6c7ee0a74d3c8597e3fdb5ce18
Jan Calta <jan.calta@here.com>
2022-06-14 00:02:20 +0200

    Add pylint config

--- test_model.py
+++ test_model.py
@@ -1,6 +1,9 @@
+from model import Batch, OrderLine
+
+
 def test_allocate_reduces_stock():
-    batch = Batch("ref1", "sku-table", 10)
-    order_line = OrderLine("sku_table", 2)
+    batch = Batch("batch-001", "sku-table", 10)
+    order_line = OrderLine("order-ref", "sku-table", 2)
 
     batch.allocate(order_line)
 
